3f559e363c659ed2b137a279eb8862ecbf161eaf
Priyanshu Belwal <priyanshubelwalpb@gmail.com>
2023-07-09 08:34:24 +0530

    Initial Commit

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+// Import packages onto app
+import express from "express";
+import morgan from "morgan";
+
+// Import routes from the ./routes
+import user from "@/routes/user-route";
+
+// Setup constant variables
+const PORT = process.env.APPLICATION_PORT || 5000;
+
+// Init express app
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Detailed server logging
+app.use(morgan("dev"));
+
+
+// Setup routing
+app.use("/users", user);
+
+// Listen to specified port in .env or default 5000
+app.listen(PORT, () => {
+  console.log(`Server is listening on: ${PORT}`);
+});
